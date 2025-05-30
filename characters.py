bcafe24d4d1750b6b4fc9525d1a936d7dca8b289
AkiPoh <59505647+AkiPoh@users.noreply.github.com>
2025-05-31 01:27:58 +0300

    Deleted render_character_row_bitmap()

--- characters.py
+++ characters.py
@@ -44,53 +44,6 @@ def render_character_bitmap(
                 )
 
 
-# # ROW BITMAP EXAMPLE
-# # INSTRUCTIONS: True means render character, False means empty space
-# # row_bitmap = [True, False, True, True]
-
-
-# def render_character_row_bitmap(
-#     screen_surface,
-#     character_bitmap,
-#     row_bitmap,
-#     spacing_between_characters,
-#     begin_position_horizontal,
-#     begin_position_vertical,
-# ):
-#     character_bitmap_width = len(character_bitmap[0])
-
-#     current_character_begin_position_horizontal = begin_position_horizontal
-
-#     for should_render_character in row_bitmap:
-#         if should_render_character:
-#             render_character_bitmap(
-#                 screen_surface,
-#                 character_bitmap,
-#                 current_character_begin_position_horizontal,
-#                 begin_position_vertical,
-#             )
-#         current_character_begin_position_horizontal += (
-#             character_bitmap_width + spacing_between_characters
-#         )
-
-
-# GRID BITMAP EXAMPLES
-# INSTRUCTIONS: Pass actual character bitmap arrays in 0D, 1D, or 2D format
-#
-# 0 DIMENSIONAL
-# grid_bitmap = character_bitmap_number_sign
-#
-# 1 DIMENSIONAL
-# grid_bitmap = [character_bitmap_number_sign, character_bitmap_space, character_bitmap_number_sign]
-#
-# 2 DIMENSIONAL
-# grid_bitmap = [
-#     [character_bitmap_number_sign, character_bitmap_space, character_bitmap_number_sign],
-#     [character_bitmap_space, character_bitmap_number_sign, character_bitmap_space],
-#     [character_bitmap_number_sign, character_bitmap_space, character_bitmap_number_sign]
-# ]
-
-
 def render_character_grid_bitmap(
     screen_surface,
     grid_bitmap,
