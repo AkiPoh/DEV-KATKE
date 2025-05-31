952965ee2ee6042ab96edf600ae1f7df8f8b5d70
AkiPoh <59505647+AkiPoh@users.noreply.github.com>
2025-05-31 21:01:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,37 +4,49 @@ def GET_DEFAULTS():
     with open(DEFAULTS_PATH) as DEFAULTS_FILE:
         DEFAULTS = json.load(DEFAULTS_FILE)
     return DEFAULTS
-DEFAULTS_PATH = "defaults_storage.json"
-DEFAULTS = GET_DEFAULTS()
+
 
 import pygame
 def PYGAME_INIT():
     pygame.init()
-PYGAME_INIT()
 
 def GET_SURFACE():
     global WIDTH_PIXELS
     global HEIGHT_PIXELS
     return pygame.display.set_mode((WIDTH_PIXELS, HEIGHT_PIXELS))
-WIDTH_PIXELS = 500
-HEIGHT_PIXELS = 300
-SURFACE = GET_SURFACE()
 
 def CREATE_CLOCK():
     return pygame.time.Clock()
-CLOCK = CREATE_CLOCK()
 
-
-
-RUNNING = True
 def EVENT_QUIT():
     global RUNNING
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             RUNNING = False
+
+def UPDATE_DISPLAY():
+    pygame.display.flip()
+
+def TICK_CLOCK():
+    global CLOCK
+    CLOCK.tick(60)  # 60 FPS LIMITED
+
+def PYGAME_QUIT():
+    pygame.quit()
+
+
+DEFAULTS_PATH = "defaults_storage.json"
+DEFAULTS = GET_DEFAULTS()
+
+PYGAME_INIT()
+WIDTH_PIXELS = 500
+HEIGHT_PIXELS = 300
+SURFACE = GET_SURFACE()
+CLOCK = CREATE_CLOCK()
+
+RUNNING = True
 while RUNNING:
     EVENT_QUIT()
-    pygame.display.flip()  # update the display
-    CLOCK.tick(60)  # limits FPS to 60
-
-pygame.quit()
+    UPDATE_DISPLAY()
+    TICK_CLOCK()
+PYGAME_QUIT()
